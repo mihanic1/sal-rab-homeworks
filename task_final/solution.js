d874ba1f817a11f25ee6d497b99f7ae384a983cd
mihanic <mihanic@yandex.ru>
2022-03-27 23:53:52 +0300

    dev5

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,23 +33,22 @@
 //    }
 // }
 
-function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
 
+function sendRequest(name, phone, address, goods, sum) {
+    let data = {order: {}, goods: []};
+    
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    data.client = `${name} ${phone}`;
+ 
+    for (let i = 0; i < countOfGoods; i += 1) { 
+        myGoods = {title: goods[i].title, count: goods[i].count}
+        data.goods.push(myGoods);
     }
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+    data.order.sum = sum;
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
-
-console.log(jsonData);
